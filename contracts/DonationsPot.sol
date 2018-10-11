bac2e727f0f9303eaea6aad0dc460e83c6f9748d
jakub-wojciechowski <jakub@alice.si>
2018-10-11 18:22:08 +0200

    Registering first donation on DonationsPot

---
+++ contracts/DonationsPot.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.4.24;
+
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+
+contract DonationsPot is Ownable {
+    using SafeMath for uint256;
+
+    address public charityManager;
+    address public tokenDistributor;
+    ERC20 public daiToken;
+
+    struct Donor {
+        string name;
+        uint256 balance;
+        uint256 donationsCount;
+    }
+
+    struct Charity {
+        string name;
+        uint256 balance;
+        uint256 donationsCount;
+    }
+
+    mapping(address => Donor) donors;
+    mapping(address => Charity) charities;
+
+    uint256 public registeredBalance;
+
+    event DonationRegistered(address from, string donorName, uint256 value);
+    event DonationTransferred(address from, string donorName, address to, string charity, uint256 value);
+
+    constructor(address _charityManager, address _tokenDistributor, ERC20 _daiToken) {
+        charityManager = _charityManager;
+        tokenDistributor = _tokenDistributor;
+        daiToken =_daiToken;
+    }
+
+    function registerDonation(address _from, string _name, uint256 _value) public onlyOwner {
+        require(daiToken.balanceOf(this).sub(registeredBalance) >= _value);
+        donors[_from] = Donor(_name, _value, 1);
+        registeredBalance = registeredBalance.add(_value);
+        emit DonationRegistered(_from, _name, _value);
+    }
+
+    function transferDonation(address _charity) public {
+
+    }
+
+    function getDonorBalance(address _donorAddress) public view returns(uint256) {
+        return donors[_donorAddress].balance;
+    }
+
+    function getDonorDonationsCount(address _donorAddress) public view returns(uint256) {
+        return donors[_donorAddress].donationsCount;
+    }
+
+
+}
---
+++ test/DonationsPot.test.js
@@ -0,0 +1,30 @@
+require('./test-utils');
+
+var DaiMock = artifacts.require("DaiMock");
+var DonationsPot = artifacts.require("DonationsPot");
+
+contract('Donations Pot', function([owner, charityManager, tokenDistributor, donor1, donor2]) {
+  var dai, pot;
+
+  before("deploy Donations Pot", async function() {
+    dai = await DaiMock.new();
+    pot = await DonationsPot.new(charityManager, tokenDistributor, dai.address);
+  });
+
+  it("should not register donation without Dai payment", async function() {
+    await pot.registerDonation(donor1, "Donor_1", 5).shouldBeReverted();
+  });
+
+
+  it("should register the first donation from Donor 1", async function() {
+    await dai.mint(pot.address, 5);
+    await pot.registerDonation(donor1, "Donor_1", 5);
+
+    (await dai.balanceOf(pot.address)).should.be.bignumber.equal(5);
+
+    (await pot.registeredBalance()).should.be.bignumber.equal(5);
+    (await pot.getDonorBalance(donor1)).should.be.bignumber.equal(5);
+    (await pot.getDonorDonationsCount(donor1)).should.be.bignumber.equal(1);
+  });
+
+});
